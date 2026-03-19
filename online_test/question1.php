d88ecc7b2ef71b2e74930f055c50ed7a41a4a37c
dev_mark <kanravee.p@hoteliers.guru>
2026-03-19 15:38:38 +0700

    edit question1

--- online_test/question1.php
+++ online_test/question1.php
@@ -51,5 +51,5 @@ $numberList = [1, 2, 3, 5, 7, 9, 2, 3, 6, 7, 2, 5, 4, 6, 1, 1, 6, 7, 3, 5, 9];
 $result = groupNumber($numberList);
 
 foreach ($result as $number => $data) {
-    echo $number . ' => count: ' . $data['count'] . ', sum: ' . $data['sum'] . '<br>';
+    echo $number . ' => count: ' . $data['count'] . ' sum: ' . $data['sum'] . '<br>';
 }
