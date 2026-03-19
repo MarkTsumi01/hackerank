dd605461e755883bbda366c93811933dbdfdf4f2
dev_mark <kanravee.p@hoteliers.guru>
2026-03-19 15:14:46 +0700

    change filename and edit

--- online_test/question2.php
+++ online_test/question2.php
@@ -1,47 +1,72 @@
 <?php
 
-$numbers = [23, 13, 56, 12, 7, 89, 33, 20, 34, 8, 66, 10, 16, 72, 4, 3, 11, 55, 16, 19, 47];
-$even = [];
-$odd = [];
+function sumList(array $numberList): int
+{
+    $sum = 0;
 
-foreach ($numbers as $number) {
-    if ($number % 2 === 0) {
+    foreach ($numberList as $number) {
+        $sum += $number;
+    }
+
+    return $sum;
+}
+
+function isEven(int $number)
+{
+    if (($number % 2) === 0) {
+        return 'true';
+    }
+
+    return 'false';
+}
+
+function isOdd(int $number)
+{
+    if (($number % 2) !== 0) {
+        return 'true';
+    }
+
+    return 'false';
+}
+
+function formatList(array $numberList): string
+{
+    $result    = '[';
+    $lastIndex = (count($numberList)) - 1;
+
+    foreach ($numberList as $index => $number) {
+        $result .= $number;
+
+        if ($index < $lastIndex) {
+            $result .= ', ';
+        }
+    }
+
+    $result .= ']';
+
+    return $result;
+}
+
+$numberList = [23, 13, 56, 12, 7, 89, 33, 20, 34, 8, 66, 10, 16, 72, 4, 3, 11, 55, 16, 19, 47];
+$even    = [];
+$odd     = [];
+
+foreach ($numberList as $number) {
+    $isEven = (($number % 2) === 0);
+
+    if ($isEven) {
         $even[] = $number;
     } else {
         $odd[] = $number;
     }
 }
 
-$sumEven = 0;
-foreach ($even as $number) {
-    $sumEven += $number;
-}
+$sumEven = sumList($even);
+$sumOdd  = sumList($odd);
+$isEven = isEven($sumEven);
+$isOdd = isOdd($sumOdd);
 
-$sumOdd = 0;
-foreach ($odd as $number) {
-    $sumOdd += $number;
-}
-
-$isEvenSumEven = ($sumEven % 2 === 0);
-$isOddSumOdd = ($sumOdd % 2 !== 0);
-
-echo "even => [";
-foreach ($even as $index => $number) {
-    echo $number;
-    if ($index < count($even) - 1) {
-        echo ", ";
-    }
-}
-echo "]\n";
-
-echo "odd  => [";
-foreach ($odd as $index => $number) {
-    echo $number;
-    if ($index < count($odd) - 1) {
-        echo ", ";
-    }
-}
-echo "]\n";
-
-echo "sumEven = " . $sumEven . " => " . ($isEvenSumEven ? "true" : "false") . "\n";
-echo "sumOdd  = " . $sumOdd  . " => " . ($isOddSumOdd  ? "true" : "false") . "\n";
+echo 'even => ' . formatList($even) . '<br>';
+echo 'odd  => ' . formatList($odd)  . '<br>';
+echo 'sumEven = ' . $sumEven . ' => ' . $isEven . '<br>';
+echo 'sumOdd  = ' . $sumOdd  . ' => ' . $isOdd;
--- online_test/question3.php
+++ online_test/question3.php
@@ -1,11 +1,11 @@
 <?php
 
-function removeUnwantTag(string $htmlString, array $allowTag): string
+function removeUnwantTag(string $htmlString, array $allowTagNameList): string
 {
     $isInsideTag = false;
     $result = '';
     $tag = '';
-    $htmlStringLength = strlen($htmlString);
+    $htmlStringLength = strlen($htmlString); 
 
     for ($index = 0; $index < $htmlStringLength; $index++) {
         $char = $htmlString[$index];
@@ -16,7 +16,7 @@ function removeUnwantTag(string $htmlString, array $allowTag): string
         } elseif ($char === '>') {
             $isInsideTag = false;
             $tag .= '>';
-            $isAllowTag = isAllowTagName($tag, $allowTag);
+            $isAllowTag = isAllowTagName($tag, $allowTagNameList); 
 
             if ($isAllowTag) {
                 $result .= $tag;
