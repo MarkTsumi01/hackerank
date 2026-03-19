b12c3dd5b5a6089fd30abf4fc1068bdb8b3f0114
dev_mark <kanravee.p@hoteliers.guru>
2026-03-19 14:27:51 +0700

    edit

--- online_test/question3.php
+++ online_test/question3.php
@@ -5,8 +5,9 @@ function removeUnwantTag(string $htmlString, array $allowTag): string
     $isInsideTag = false;
     $result = '';
     $tag = '';
+    $htmlStringLength = strlen($htmlString);
 
-    for ($index = 0; isset($htmlString[$index]); $index++) {
+    for ($index = 0; $index < $htmlStringLength; $index++) {
         $char = $htmlString[$index];
 
         if ($char === '<') {
@@ -15,8 +16,9 @@ function removeUnwantTag(string $htmlString, array $allowTag): string
         } elseif ($char === '>') {
             $isInsideTag = false;
             $tag .= '>';
+            $isAllowTag = isAllowTag($tag, $allowTag);
 
-            if (isAllowTag($tag, $allowTag)) {
+            if ($isAllowTag) {
                 $result .= $tag;
             }
 
@@ -31,7 +33,7 @@ function removeUnwantTag(string $htmlString, array $allowTag): string
     return $result;
 }
 
-function isAllowTag(string $tag, array $allowTagNameList): bool 
+function isAllowTag(string $tag, array $allowTagNameList): bool
 {
     $tagName = extractTagName($tag);
 
@@ -44,19 +46,20 @@ function isAllowTag(string $tag, array $allowTagNameList): bool
     return false;
 }
 
-function extractTagName(string $tag): string 
+function extractTagName(string $tag): string
 {
     $name = '';
     $start = 0;
-    
-    if (isset($tag[1]) === '/') {
+    $tagLength = strlen($tag);
+
+    if ($tag[1] === '/') {
         $start = 2;
     } else {
         $start = 1;
     }
 
-    for ($index = $start; isset($tag[$index]); $index++){
-        $char = $tag[$index]; 
+    for ($index = $start; $index < $tagLength; $index++) {
+        $char = $tag[$index];
 
         if ($char === '/' || $char === '>' || $char === ' ') {
             break;
@@ -71,6 +74,6 @@ function extractTagName(string $tag): string
 $htmlString = '<div><h2>What is Lorem Ipsum?</h2><h3>What is Lorem Ipsum?</h3><span>What is Lorem Ipsum?</span><p><strong>Lorem Ipsum</strong> is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.<br /> <br />960s with the versions of Lorem Ipsum.</p></div>';
 $allowTagNameList = ['br', 'p'];
 
-$result = removeUnwantTag($htmlString,$allowTagNameList);
+$result = removeUnwantTag($htmlString, $allowTagNameList);
 
 echo $result;
